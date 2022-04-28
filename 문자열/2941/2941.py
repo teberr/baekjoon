711c51936878d2b0fcde72f80b6e4d1cc4a7e0c1
teberr <43701168+teberr@users.noreply.github.com>
2022-04-29 00:06:03 +0900

    Update 2941.py
    더 나은 방안 추가

--- 문자열/2941/2941.py
+++ 문자열/2941/2941.py
@@ -13,3 +13,15 @@ for i in croatia:
 
 text=text.replace(" ","")
 print(len(text)+count)
+
+''' 
+count랑 공백을 아예 한번에 끝내는게 더 효율적
+
+for i in croatia:
+        text=text.replace(i,'*')
+이렇게 하면 크로아티아 알파벳이 전부 한글자인 *로 치환되므로 전부 치환된 후 길이를 구하면 한번에 구할 수 있음.
+
+
+
+
+'''
