1367a81662382cc2312d3925bba9f5adc6a7e82f
teberr <43701168+teberr@users.noreply.github.com>
2022-06-10 18:25:47 +0900

    Create 15650.py

---
+++ 백트래킹/15650.py
@@ -0,0 +1,15 @@
+import sys
+N,M=map(int,sys.stdin.readline().split())
+number=[]
+
+    
+def dfs(index):
+    if len(number)==M:
+        for x in number:
+            print(x,end=" ")
+        print()
+    for i in range(index,N+1):
+        number.append(i)
+        dfs(i+1)
+        number.pop()
+dfs(1)
